78cf3f7365b6571b9ffa044f7a795a88325c5618
tehpsalmist <bstewardcodes@gmail.com>
2019-04-28 17:28:55 -0400

    score previewer spacing

--- vbs-scorekeeper/src/components/ScoresPreviewer.js
+++ vbs-scorekeeper/src/components/ScoresPreviewer.js
@@ -9,24 +9,24 @@ export const ScoresPreviewer = ({ close, scores }) => {
     <button className='text-5xl ml-4 p-4 fixed top-0 left-0' onClick={close}>{'\u2715'}</button>
     <div
       onClick={e => e.stopPropagation()}
-      className='shadow-lg rounded-lg bg-gray-600 text-white w-5xl py-4 opacity-100 z-50 flex flex-wrap'
+      className='shadow-lg rounded-lg bg-gray-600 text-white w-5xl py-12 opacity-100 z-50 flex flex-wrap'
     >
       <h1 className='text-4xl text-center w-full'>Scores Preview</h1>
       {Object.keys(scores).map(team => {
-        const classes = `w-1/2 flex flex-col px-12 text-shadow-${mappings.colors[team]}`
+        const classes = `w-1/2 flex flex-col px-20 text-shadow-${mappings.colors[team]}`
         return <div className={classes}>
           <h2 className='text-3xl text-center'>{mappings.names[team]}</h2>
           <ul>
             {Object.keys(scores[team]).map(category => {
-              return <li className='text-2xl py-2'>
-                <em>{mappings.names[category]}</em>
-                {`: ${scores[team][category].toLocaleString()}`}
+              return <li className='text-2xl py-2 flex justify-between'>
+                <em>{mappings.names[category]}:</em>
+                <span>{scores[team][category].toLocaleString()}</span>
               </li>
             })}
             <strong>
-              <li className='text-2xl py-2'>
-                <em>Total</em>
-                {`: ${Object.keys(scores[team]).reduce((total, cat) => total + scores[team][cat], 0).toLocaleString()}`}
+              <li className='text-2xl py-2 flex justify-between'>
+                <em>Total:</em>
+                <span>{Object.keys(scores[team]).reduce((total, cat) => total + scores[team][cat], 0).toLocaleString()}</span>
               </li>
             </strong>
           </ul>
